2d3df785b3cc8ed9251618198395aedd77ec6f22
Masato INOUE <masainox@gmail.com>
2011-05-23 23:43:58 +0900

    Add v0.0.1 code.

---
+++ test/sample.js
@@ -0,0 +1,26 @@
+
+var client = require('../lib/client');
+var OAuth = require('oauth').OAuth;
+
+var consumerKey = 'YOUR_CONSUMER_KEY';
+var consumerSecret = 'YOUR_CONSUMER_SECRET';
+var accessToken = 'YOUR_ACCESS_TOKEN';
+var accessTokenSecret = 'YOUR_ACCESS_TOKEN_SECRET';
+
+var oauth = new OAuth('http://twitter.com/oauth/request_token',
+                               'http://twitter.com/oauth/access_token',
+                                consumerKey, consumerSecret,
+                                '1.0', null, 'HMAC-SHA1');
+
+var access = {token: accessToken, tokenSecret: accessTokenSecret};
+tw = new client.Client(oauth, access);
+
+tw.on('onUserTimeline', function(err, data) {
+    if (err) {
+        console.log('error');
+    } else {
+        console.log(data);
+    }
+});
+
+tw.userTimeline();
